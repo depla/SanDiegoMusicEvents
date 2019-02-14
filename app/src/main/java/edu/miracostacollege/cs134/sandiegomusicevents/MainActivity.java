82434a1c09534edcc8fd2eb868ff37b1051ca059
4803AW16 <4803AW16@usa.miracosta.cc.ca.us>
2019-02-14 14:46:50 -0800

    Initial commit

---
+++ app/src/main/java/edu/miracostacollege/cs134/sandiegomusicevents/MainActivity.java
@@ -0,0 +1,56 @@
+package edu.miracostacollege.cs134.sandiegomusicevents;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import edu.miracostacollege.cs134.sandiegomusicevents.model.MusicEvent;
+
+public class MainActivity extends ListActivity {
+
+    private ListView eventsListView;
+
+
+    //position is what the user clicked ie ariana grande is position 3
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        //extract the info we need
+        String artist = MusicEvent.titles[position];
+        String details = MusicEvent.details[position];
+
+        //lets make the intent
+        //navigate from this class to the EventDetailsActivity class
+        Intent detailsIntent = new Intent(this, EventDetailsActivity.class);
+        detailsIntent.putExtra("Artist", artist);
+        detailsIntent.putExtra("Details", details);
+
+        //start the activity
+        startActivity(detailsIntent);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //since the layout is being inflated with "android.R.layout.simple_expandable_list_item_1" ,
+        // DON'T set the content view
+        //setContentView(R.layout.activity_main);
+
+        eventsListView = findViewById(R.id.eventsListView);
+
+        //connect the listview with an ArrayAdpater to fill out the data
+                                                                //this = show in this activity  simple..= layout item = text
+        ArrayAdapter<String> eventsAdapter =
+                new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, MusicEvent.titles);
+
+        //set the adapter
+        setListAdapter(eventsAdapter);
+
+    }
+}
